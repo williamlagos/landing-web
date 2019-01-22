ff86085152ac6d882ddbe8e7105b575fd5ee8395
William Oliveira de Lagos <william.lagos@icloud.com>
2019-01-22 16:44:57 -0200

    Added source tree

---
+++ src/components/layout.js
@@ -0,0 +1,5 @@
+import React from "react"
+import "../styles/normalize.css"
+import "../styles/skeleton.css"
+
+export default ({ children }) => <div>{children}</div>
---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/layout.js"
+
+class Main extends React.Component {
+  render() {
+    return (
+      <Layout>
+        <div className="row" style={{ "height": "100%" }}>
+          <p style={{ "text-align": "center" }}>Efforia Cloud</p>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default Main
